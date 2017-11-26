a8d745df27a89977457389142c8b71cc5de7c87b
RONTheCookie <ronthecookie0101@gmail.com>
2017-11-26 16:13:41 +0200

    smarter ping command

--- commands/ping.cmd.js
+++ commands/ping.cmd.js
@@ -1,6 +1,7 @@
 module.exports = {
-    run: (msg, args, client) => {
-        msg.channel.send("Pong. :ping_pong:");
+    run: async (msg, args, client) => {
+        let m = await msg.channel.send("Pong! :ping_pong:");
+        m.edit(`Pong! :ping_pong: (Roundtrip: ${m.createdTimestamp - msg.createdTimestamp}ms | One-way: ${~~main.bot.ping}ms)`);
     },
     meta: {
         name: "ping",
