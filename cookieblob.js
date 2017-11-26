091119ca473c9fe01b6427da61dadccf68d9eaba
RONTheCookie <ronthecookie0101@gmail.com>
2017-11-26 15:08:30 +0200

    simple d.js setup

--- cookieblob.js
+++ cookieblob.js
@@ -1,2 +1,14 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
+const config = require("./config.json");
+let commands = {};
+client.login(config.token);
+client.on('ready',()=>{
+    console.log(`Logged in as ${client.user.tag}`);
+});
+
+module.exports = {
+    config:config,
+    client:client,
+    commands:commands
+}
