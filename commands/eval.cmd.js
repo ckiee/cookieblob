db227f19bc2853b500432462485dc64e17cc5778
RONTheCookie <ronthecookie0101@gmail.com>
2017-11-27 17:20:57 +0200

    oh im dumb

--- commands/eval.cmd.js
+++ commands/eval.cmd.js
@@ -2,7 +2,7 @@ module.exports = {
     run: async (msg, args, client) => {
         try {
         const toEval = args.join(" ");
-        let result = eval(code);
+        let result = eval(toEval);
         if (typeof result != "string") result = require("util").inspect(evaled);
         if (result.split("") > 1999) return msg.channel.send("Message is over the discord message contents limit.");
         msg.channel.send("```"+result+"```");
