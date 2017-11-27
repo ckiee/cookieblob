bbec391c380f7d599f3b09337a56a52a175b7d83
RONTheCookie <ronthecookie0101@gmail.com>
2017-11-27 15:04:50 +0200

    add field to about embed

--- commands/about.cmd.js
+++ commands/about.cmd.js
@@ -1,5 +1,6 @@
 const RichEmbed = require("discord.js").RichEmbed;
 const config = require("../cookieblob").config;
+const github_emote = "<:github:384690138749468682>";
 module.exports = {
     run: async (msg, args, client) => {
         msg.channel.send(new RichEmbed()
@@ -10,6 +11,8 @@ module.exports = {
         .addField("Let's help you get started!",`Type ${config.prefix}help to see a list of commands!
 Good luck 😃`)
         .addField("Want to invite me?","Click [here](https://discordapp.com/oauth2/authorize?client_id=324874714646577152&scope=bot&permissions=3173376)!")
+        .addField("Learn more?",
+`[${github_emote} Github](https://github.com/ronthecookie/cookieblob)`)
     );
     },
     meta: {
