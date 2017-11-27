2a8e642fef85c845fa1fd96faf3fdab83b95b58f
RONTheCookie <ronthecookie0101@gmail.com>
2017-11-27 15:24:03 +0200

    Oh.

--- commands/8ball.cmd.js
+++ commands/8ball.cmd.js
@@ -1,7 +1,7 @@
 const ball_choices = ["It is certain","It is decidedly so","Without a doubt","Yes definitely","You may rely on it","As I see it, yes","Most likely","Outlook good","Yes","Signs point to yes","Reply hazy try again","Ask again later","Better not tell you now","Cannot predict now","Concentrate and ask again","Don't count on it","My reply is no","My sources say no","Outlook not so good","Very doubtful"];
 module.exports = {
     run: async (msg, args, client) => {
-        if (args.length < 1) require("../util").invalidUsageEmbed(msg,"8ball");
+        if (args.length < 1) return msg.channel.send(require("../util").invalidUsageEmbed(msg,"8ball"));
         msg.channel.send(`:8ball: ${ball_choices[getRandomInt(0,ball_choices.length)]}.`);
     },
     meta: {
