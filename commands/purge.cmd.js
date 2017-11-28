4b3ab7a8148d57033c1c6165a09ee98b656a8488
RONTheCookie <ronthecookie0101@gmail.com>
2017-11-28 17:10:19 +0200

    Finish 'purge' command woo!

--- commands/purge.cmd.js
+++ commands/purge.cmd.js
@@ -1,6 +1,13 @@
 module.exports = {
     run: async (msg, args, client) => {
-        msg.channel.send("You have permission!");
+        if (!msg.guild.me.hasPermission("MANAGE_MESSAGES")) return msg.channel.send(":x: Cookieblob does not have the `Manage Messages` permission.");
+        if (args.length != 1) msg.channel.send(require("../util").invalidUsageEmbed(msg, "purge"));
+        let count = parseInt(args[0]);
+        if (count == null) msg.channel.send(require("../util").invalidUsageEmbed(msg, "purge"));
+        let deleted = await msg.channel.bulkDelete(count);
+        msg.channel.send(`:ok_hand: Removed ${deleted} messages.`).then(m => {
+            m.delete(1000*30);
+        });
     },
     meta: {
         name: "purge",
