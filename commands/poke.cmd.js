233547453f053230c4e0954162edcf112bd17ed2
RONTheCookie <ronthecookie0101@gmail.com>
2017-12-01 13:34:33 +0200

    add poke command

---
+++ commands/poke.cmd.js
@@ -0,0 +1,20 @@
+const {Message, Client} = require("discord.js");
+module.exports = {
+    /**
+     * @param {Message} msg
+     * @param {Array<String>} args
+     * @param {Client} client
+     */
+    run: async (msg, args, client) => {
+        msg.channel.send(`*${msg.author.tag} poked ${msg.mentions.users.first()}`)
+        if (msg.mentions.members.first() == null) return msg.channel.send(require("../util").invalidUsageEmbed(msg, "poke"));
+        msg.channel.send(`*${msg.author.tag} poked ${msg.mentions.users.first().tag}!`);
+    },
+    meta: {
+        name: "poke",
+        description: "Poke a user!",
+        usage: ["user mention"],
+        permissionLevel:0,
+        guildOnly:true
+    }
+}
