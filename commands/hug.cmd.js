904bfcb1b528c575825e7646f5de46769702675e
RONTheCookie <ronthecookie0101@gmail.com>
2017-12-01 13:35:27 +0200

    add 'hug' command

---
+++ commands/hug.cmd.js
@@ -0,0 +1,20 @@
+const {Message, Client} = require("discord.js");
+module.exports = {
+    /**
+     * @param {Message} msg
+     * @param {Array<String>} args
+     * @param {Client} client
+     */
+    run: async (msg, args, client) => {
+        msg.channel.send(`*${msg.author.tag} hugs ${msg.mentions.users.first()}`)
+        if (msg.mentions.members.first() == null) return msg.channel.send(require("../util").invalidUsageEmbed(msg, "hug"));
+        msg.channel.send(`*${msg.author.tag} hugs ${msg.mentions.users.first().tag}!`);
+    },
+    meta: {
+        name: "hug",
+        description: "Hug a user! Lovely.",
+        usage: ["user mention"],
+        permissionLevel:0,
+        guildOnly:true
+    }
+}
