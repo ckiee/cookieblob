d7c92418070b91839e717ab63c2e26b9bdfd5226
RONTheCookie <ronthecookie0101@gmail.com>
2017-12-03 15:35:33 +0200

    beepboop

--- commands/setmodrole.cmd.js
+++ commands/setmodrole.cmd.js
@@ -2,12 +2,12 @@ const datastorage = require("../datastorage");
 module.exports = {
     run: async (msg, args, client) => {
         if (args.length < 1) return msg.channel.send(require("../util").invalidUsageEmbed(msg, "setmodrole"));
-        let gd = datastorage.getGuildData(msg.guild.id);
-        await gd.ready;
         let role = msg.guild.roles.find("name",args.join(" "));
         if (role == null) return msg.channel.send(":x: Invalid role!");
+        let gd = datastorage.getGuildData(msg.guild.id);
+        await gd.ready;
         gd.guildData.modRole = role.id;
-        gd.updateToDB();
+        await gd.updateToDB();
         msg.channel.send(`:ok_hand: Updated mod role to '${role.name}'.`);
     },
     meta: {
