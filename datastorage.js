3f11516774d60197207cf30b153e22d731659887
RONTheCookie <ronthecookie0101@gmail.com>
2017-12-05 14:25:02 +0200

    remove/comment out debug

--- datastorage.js
+++ datastorage.js
@@ -4,7 +4,7 @@ let guildDataClassInstances = {};
 let guilds = r.table("guildData");
 const updateLocalConnection = (cnct)=>{
     connection = cnct;
-    r.table("guildData").run(connection).then(result => {console.log('guild data dump:');result.toArray().then((betterresult)=>{console.log(betterresult)})})
+    // r.table("guildData").run(connection).then(result => {console.log('guild data dump:');result.toArray().then((betterresult)=>{console.log(betterresult)})})
 }
 /**
  * Class for guild data.
@@ -21,10 +21,7 @@ class GuildData {
             this.updateFromDB().then(()=>{
                 if (this.guildData == null) {
                     this.makeDefaultSettings();
-                    guilds.insert(this.guildData).run(connection).then(res=>{
-                        console.log(res);
-                    });
-                    console.log(`[datastorage] created new entry for guild ${this.guildID} with data ${require("util").inspect(this.guildData)} `);
+                    guilds.insert(this.guildData).run(connection);
                 }
             }).then(resolve).catch(reject);
         });
@@ -34,14 +31,12 @@ class GuildData {
      */
     async updateFromDB() {
         this.guildData = await guilds.get(this.guildID).run(connection);
-        console.log('get',this.guildID, this.guildData);
     }
     /**
      * Send all of our updates to the database, if there are any.
      */
     async updateToDB() {
         let result = await guilds.get(this.guildID).update(this.guildData).run(connection);
-        console.log('update',this.guildID, result, this.guildData);
         return result;
     }
     /**
