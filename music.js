0da7366f01ed07886ecda76945a408950a2f6815
RONTheCookie <ronthecookie0101@gmail.com>
2017-12-14 17:49:21 +0200

    debug

--- music.js
+++ music.js
@@ -96,6 +96,7 @@ async function play(msg) {
     if (voiceChannel == null) voiceChannel = await msg.member.voiceChannel.join();
     let sq = mg.shiftQueue();
     mg.setDispatcher(voiceChannel.playStream(ytdl(sq.youtube.link,{filter:"audio"}),{passes:5}));
+    console.log('playing');
     mg.getDispatcher().on('end',reason => {
         setTimeout(()=>{
             let sqa = mg.queue[0];
