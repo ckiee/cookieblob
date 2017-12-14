4850662b1b29d8bb91a74672904f06c3fdb8a1c5
RONTheCookie <ronthecookie0101@gmail.com>
2017-12-14 20:06:49 +0200

    test rl

--- commands/ping.cmd.js
+++ commands/ping.cmd.js
@@ -1,7 +1,7 @@
 module.exports = {
     run: async (msg, args, client) => {
         let m = await msg.channel.send("Pong! :ping_pong:");
-        m.edit(`Pong! :ping_pong: (Roundtrip: ${m.createdTimestamp - msg.createdTimestamp}ms | One-way: ${~~client.ping}ms)`);
+        m.edit(`Pong!!! :ping_pong: (Roundtrip: ${m.createdTimestamp - msg.createdTimestamp}ms | One-way: ${~~client.ping}ms)`);
     },
     meta: {
         name: "ping",
