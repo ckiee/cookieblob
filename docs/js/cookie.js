23e6da5209de8d3f3cd1265438e401309b8becc8
RONTheCookie <ronthecookie0101@gmail.com>
2017-12-15 12:47:26 +0200

    [web] auto copyright year

---
+++ docs/js/cookie.js
@@ -0,0 +1,4 @@
+let cpyr = "Copyright © "+new Date().getFullYear()+"RONTheCookie. All rights reserved.";
+window.onload(function(event){
+    document.querySelector("#actual-copyright").innerHTML = cpyr;
+});
