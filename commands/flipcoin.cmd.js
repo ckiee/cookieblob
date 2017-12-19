e246aa855b9de9604a75c4dbb028898a97e23634
RONTheCookie <ronthecookie0101@gmail.com>
2017-12-19 21:11:34 +0200

    debug

--- commands/flipcoin.cmd.js
+++ commands/flipcoin.cmd.js
@@ -1,10 +1,11 @@
 module.exports = {
     run: async (msg, args, client) => {
         let v;
-        if (Math.random() >= 0.5) {
+        let r = Math.random();
+        if (r >= 0.5) {
             v = "Heads";
         } else v = "Tails";
-        msg.channel.send(`The coin landed on *${v}*.`);
+        msg.channel.send(`The coin landed on *${v}*. ${r}`);
     },
     meta: {
         name: "flipcoin",
