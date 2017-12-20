d3718de3223b22d111c472253af0fd124403c727
RONTheCookie <ronthecookie0101@gmail.com>
2017-12-20 13:45:06 +0200

    fix delete

--- commands/removeissue.cmd.js
+++ commands/removeissue.cmd.js
@@ -9,7 +9,7 @@ module.exports = {
     run: async (msg, args, client) => {
         if (args.length < 1) return msg.channel.send(require("../util").invalidUsageEmbed(msg, "removeissue"));
         let id = args[0];
-        let d = await client.guilds.get("344798017611759638").channels.get("382554148878548995").messages.fetch(id);
+        let d = await client.guilds.get("392987506670305281").channels.get("392992243020988416").messages.fetch(id);
         if (d == null) {
             msg.channel.send(":x: that issue does not exist.");
             return;
