7e0965426ba6b7e152e6598b11b13acb54a58f42
RONTheCookie <ronthecookie0101@gmail.com>
2017-12-22 14:43:04 +0200

    pos

--- commands/ping.cmd.js
+++ commands/ping.cmd.js
@@ -6,8 +6,8 @@ module.exports = {
      * @param {Client} client
      */
     run: async (msg, args, client) => {
-        let m = await msg.channel.send("Pong! :ping_pong:");
-        m.edit(`<a:loadingrolling:393744853684584448> Pong! :ping_pong: (Roundtrip: ${m.createdTimestamp - msg.createdTimestamp}ms | One-way: ${~~client.ping}ms)`);
+        let m = await msg.channel.send("Pong! <a:loadingrolling:393744853684584448>");
+        m.edit(`Pong! <a:loadingrolling:393744853684584448> (Roundtrip: ${m.createdTimestamp - msg.createdTimestamp}ms | One-way: ${~~client.ping}ms)`);
     },
     meta: {
         name: "ping",
