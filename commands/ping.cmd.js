1fd9f6b4561b5929be789aac94bafcec3f748b9b
RONTheCookie <ronthecookie0101@gmail.com>
2017-12-22 14:42:07 +0200

    add emote because why not

--- commands/ping.cmd.js
+++ commands/ping.cmd.js
@@ -7,7 +7,7 @@ module.exports = {
      */
     run: async (msg, args, client) => {
         let m = await msg.channel.send("Pong! :ping_pong:");
-        m.edit(`Pong! :ping_pong: (Roundtrip: ${m.createdTimestamp - msg.createdTimestamp}ms | One-way: ${~~client.ping}ms)`);
+        m.edit(`<a:loadingrolling:393744853684584448> Pong! :ping_pong: (Roundtrip: ${m.createdTimestamp - msg.createdTimestamp}ms | One-way: ${~~client.ping}ms)`);
     },
     meta: {
         name: "ping",
