2fa2d7510f3300614d883ac5916a3b14664d04b8
RONTheCookie <ronthecookie0101@gmail.com>
2017-12-23 10:22:23 +0200

    [help] start rewrite

--- commands/help.cmd.js
+++ commands/help.cmd.js
@@ -15,60 +15,11 @@ module.exports = {
         let currentPage = 0;
         const controlArrow = "▶";
         const backwardsArrow = "◀";
-        async function makeEmbed(page) {
-            let startFrom = page*cpp;
-            let pageCmds = commands.slice(startFrom, startFrom + cpp);
-            let embed = new MessageEmbed()
-            .setAuthor("Cookieblob command list - Page "+(currentPage+1),msg.author.avatarURL)
-            .setColor(0xffc300)
-            .setTimestamp(new Date());
-            pageCmds.forEach(cmd => {
-                embed.addField(cmd.meta.name,`Description: \`${cmd.meta.description}\` 
-Usage: \`${require("../util").renderUsage(cmd.meta.name)}\``);                
-            });
-            return embed;
-        }
-        let m = await msg.channel.send(await makeEmbed(currentPage/*should be 0*/));
-        async function makeCollector(backwards) {
-            const arrow = backwards?backwardsArrow:controlArrow;
-        await m.react(arrow);
-        const collector = m.createReactionCollector(
-            (reaction, user) => reaction.emoji.name == arrow && user.id != m.author.id,
-            {time: abandonTime}
-        );
-        collector.on('collect', async r => {
-            let nextPage = backwards ? currentPage - 1: currentPage + 1;
-            let pageCmds = commands.slice(nextPage*cpp, nextPage*cpp + cpp);
-            if (pageCmds.length == 0) {
-                let xOm = await msg.channel.send(`:x: This page is the ${backwards?"first":"last"} page.`);
-                await r.remove(msg.author);
-                setTimeout(async()=>{await xOm.delete();}, 1000*30);
-                return;
-            }
-            collector.stop("ignoreMeCookieblob");
-            if (backwards) currentPage--; else currentPage++;
-            await r.remove(msg.author);
-            let newEmbed = await makeEmbed(currentPage);
-            await m.edit(newEmbed);
-            await makeCollector(backwards);
-        });
-        collector.on('end', async (coll, reason) => {
-            if (reason != "time") return;
-            await m.delete();
-        });
-    }
-    await makeCollector(true);
-    await makeCollector(false);
-    const xEmote = "❌";
-    m.react(xEmote);
-    const xColl = m.createReactionCollector(
-        (reaction, user) => reaction.emoji.name == xEmote && user.id != m.author.id,
-        {time: abandonTime}
-    );
-    xColl.on('collect', async xG => {
-        if (msg.deletable) msg.delete("user request");
-        m.delete();
-    });
+        let embed = new MessageEmbed()
+        .setAuthor("Cookieblob command list - Page "+(currentPage+1),msg.author.avatarURL)
+        .setColor(0xffc300)
+        .setTimestamp(new Date());
+        const xEmote = "❌";
     },
     meta: {
         name: "help",
