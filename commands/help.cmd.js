842de59718ff1be0ce469d75e31c9a2e22288a74
RONTheCookie <ronthecookie0101@gmail.com>
2017-12-23 10:51:46 +0200

    [help.cmd.js] Make it more simpler. No more reactions.

--- commands/help.cmd.js
+++ commands/help.cmd.js
@@ -7,15 +7,24 @@ module.exports = {
      * @argument {Array<String>} args 
      */
     run: async (msg, args, client) => {
-        if (!msg.guild.me.hasPermission("ADD_REACTIONS")) return msg.channel.send(":x: I need `Add Reactions` permissions for this! (So you can use my page system)");
-        if (!msg.guild.me.hasPermission("MANAGE_MESSAGES")) return msg.channel.send(":x: I need `Manage Messages` permissions for this! (So I can remove your reactions for my page system!)");
+        if (args.length < 1) return msg.channel.send(require("../util").invalidUsageEmbed(msg, "help"));
+        if (args[0] < 1) return msg.channel.send(":x: Pages start from `1`!");
         const abandonTime = 120000;//ms
         const cpp = 10; // commands per page
         const commands = Object.keys(cookieblob.commands).map(cookieblob.getCommand).filter(cm => cm.meta.permissionLevel != "botAdmin").filter(cx => cx.meta.permissionLevel != "botOwner");
-        let currentPage = 0;
-        const controlArrow = "▶";
-        const backwardsArrow = "◀";
-        const xEmote = "❌";
+        let currentPage = args[0] - 1;
+        function isEmptyPage(page) {
+            let startFrom = page*cpp;
+            let pageCmds = commands.slice(startFrom, startFrom + cpp);
+            return args.length == 0;
+        }
+        function getLastProperPage() {
+            let amount = commands.length;
+            let result = commands.length/cpp;
+            if (commands.length%cpp != 0) result++;
+            return result;
+        }
+        if (isEmptyPage(currentPage)) return msg.channel.send(`Invalid page! Range is 1 to ${getLastProperPage()}`);
         async function makeEmbed() {
             let startFrom = currentPage*cpp;
             let pageCmds = commands.slice(startFrom, startFrom + cpp);
@@ -31,47 +40,11 @@ Usage: \`${require("../util").renderUsage(cmd.meta.name)}\``);
         }
         let emB = await makeEmbed();
         let m = await msg.channel.send(emB);
-        await m.react(backwardsArrow);
-        await m.react(controlArrow);
-        await m.react(xEmote);
-        async function movePage(backwards) {
-            if (backwards) currentPage--;
-            else currentPage++;
-            let emBn = await makeEmbed();
-            await m.edit(emBn);
-        }
-        async function makePageMoveCollector(backwards) {
-            let collector = m.createReactionCollector(
-                (reaction, user) => reaction.emoji.name == backwards?backwardsArrow:controlArrow && user.id === msg.author.id,
-                {time:abandonTime}
-            );
-            collector.on('collect', async r => {
-                await movePage(backwards);
-                await r.remove(msg.author.id);
-            });
-            collector.on('end', async collected => {
-               await m.delete(); 
-            });
-        }
-
-        await makePageMoveCollector(true);
-        await makePageMoveCollector(false);
-        let exitCollector = m.createReactionCollector(
-            (reaction, user) => reaction.emoji.name == xEmote && user.id === msg.author.id,
-            {time:abandonTime}
-        );
-        exitCollector.on('collect', async r => {
-            if (msg.deletable) await msg.delete();
-            if (m.deleteable) await m.delete();
-        });
-        exitCollector.on('end', async collected => {
-           await m.delete(); 
-        });
     },
     meta: {
         name: "help",
         description: "Show the list of commands you are currently looking at!",
-        usage: [],
+        usage: ["page:number"],
         permissionLevel:0,
         guildOnly:false
     }
