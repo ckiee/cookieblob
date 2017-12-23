4808bf3ceea7e55f01cb2df5832e82d1f8b46d6d
jellz <24366082+jellz@users.noreply.github.com>
2017-12-23 14:52:53 +1100

    try this

--- commands/suggest.js
+++ commands/suggest.js
@@ -9,7 +9,7 @@ module.exports = {
     run: async (msg, args, client) => {
         if (args.length < 1) return msg.channel.send(require("../util").invalidUsageEmbed(msg, "suggest"));
         let suggestion = args.join(" ");
-        let m = await client.channels.get("393971596177702922")
+        let m = await client.guilds.get("392987506670305281").channels.get("393971596177702922")
         .send(`:tada: new suggestion! from ${msg.author.tag} (${msg.author.id}) content: \`${suggestion}\` `);
         msg.channel.send(`:ok_hand: Submitted suggestion: \`${suggestion}\` Your feedback is greatly appreciated!`);
     },
