af76db5dbdb59fbd2355d73268a36cb911c97792
RONTheCookie <ronthecookie0101@gmail.com>
2017-12-23 13:22:54 +0200

    docs

--- site/site.js
+++ site/site.js
@@ -1,11 +1,12 @@
 const express = require("express");
+const cookieblob = require("../cookieblob");
 let app = express();
 app.set('view engine', 'ejs');
 app.get("/", (req, res)=>{
-    res.render("index.ejs", {guildAmount:require("../cookieblob").client.guilds.size, cookieblob:require("../cookieblob")});
+    res.render("index.ejs", {guildAmount:cookieblob.client.guilds.size});
 });
 app.get("/docs", (req, res)=>{
-    res.render("docs.ejs", {cookieblob:require("../cookieblob"), commands: Object.keys(cookieblob.commands).map(cookieblob.getCommand).filter(cm => cm.meta.permissionLevel != "botAdmin").filter(cx => cx.meta.permissionLevel != "botOwner")});
+    res.render("docs.ejs", {renderUsage: require("../util").renderUsage, commands: Object.keys(cookieblob.commands).map(cookieblob.getCommand).filter(cm => cm.meta.permissionLevel != "botAdmin").filter(cx => cx.meta.permissionLevel != "botOwner")});
 });
 app.use(express.static("static"));
 app.listen(8085, function() {
