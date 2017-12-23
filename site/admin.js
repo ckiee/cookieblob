f125f31717a7450c7c24b8f1b2512d6608cd3077
RONTheCookie <ronthecookie0101@gmail.com>
2017-12-23 15:04:20 +0200

    setup admin

--- site/admin.js
+++ site/admin.js
@@ -3,3 +3,6 @@ const app = require("./site").app;
 let oauthguard = require("./oauthguard");
 let router = express.Router();
 oauthguard.router.use("/admin", router);
+router.get("/", (req, res)=>{
+    res.render("admin/index");
+});
