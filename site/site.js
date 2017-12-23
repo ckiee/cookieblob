914a4484a71d70c0eb59bb3ee583144578fd53ac
RONTheCookie <ronthecookie0101@gmail.com>
2017-12-23 12:59:14 +0200

    setup docs page in njs

--- site/site.js
+++ site/site.js
@@ -2,7 +2,10 @@ const express = require("express");
 let app = express();
 app.set('view engine', 'ejs');
 app.get("/", (req, res)=>{
-    res.render("index.ejs", {guildAmount:require("../cookieblob").client.guilds.size});
+    res.render("index.ejs", {guildAmount:require("../cookieblob").client.guilds.size, cookieblob:require("../cookieblob")});
+});
+app.get("/docs", (req, res)=>{
+    res.render("docs.ejs", {cookieblob:require("../cookieblob"), commands: Object.keys(cookieblob.commands).map(cookieblob.getCommand).filter(cm => cm.meta.permissionLevel != "botAdmin").filter(cx => cx.meta.permissionLevel != "botOwner")});
 });
 app.use(express.static("static"));
 app.listen(8085, function() {
