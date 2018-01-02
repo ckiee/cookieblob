548b25fafb335aceb648e4fe469f1e53585bc95d
ronthecookie <ronthecookie0101@gmail.com>
2018-01-02 14:40:02 +0200

    add qr cmd

---
+++ commands/qr.cmd.js
@@ -0,0 +1,21 @@
+const {Client, Message, MessageAttachment}  = require("discord.js");
+const qr = require("qr-image");
+module.exports = {
+                /**
+     * @param {Message} msg
+     * @param {Array<String>} args
+     * @param {Client} client
+     */
+    run: async (msg, args, client) => {
+        let c = args.join(" ");
+        let m = await msg.channel.send("<a:loadingrolling:393744853684584448> Generating QR code.");
+        await m.edit(new MessageAttachment(qr.image(c, {type:"png"})));
+    },
+    meta: {
+        name: "qr",
+        description: "Make a QR Code.",
+        usage: [],
+        permissionLevel:0,
+        guildOnly:false
+    }
+}
