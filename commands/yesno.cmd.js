ee5371d470194f863e23d1ae697bff309575eb68
ronthecookie <ronthecookie0101@gmail.com>
2018-01-11 15:04:36 +0200

    add 'yesno' cmd

---
+++ commands/yesno.cmd.js
@@ -0,0 +1,26 @@
+const {Client, Message, MessageEmbed}  = require("discord.js");
+const request = require("request");
+module.exports = {
+                /**
+     * @param {Message} msg
+     * @param {Array<String>} args
+     * @param {Client} client
+     */
+    run: async (msg, args, client) => {
+        let m = await msg.channel.send(new MessageEmbed().setDescription("<a:loadingrolling:393744853684584448>"));
+        request.get("https://yesno.wtf/api/", {
+            json:true,
+        }, async (error, response, body) => {
+            let respo = JSON.parse(body);
+            await m.edit(new MessageEmbed().setAuthor(msg.author.tag, msg.author.displayAvatarURL()).setColor(0xadf442)
+            .setImage(respo.image).setTitle(respo.answer).setTimestamp(new Date()).setFooter("Supplied by https://yesno.wtf/"));
+        });
+    },
+    meta: {
+        name: "yesno",
+        description: "Yes, or No?",
+        usage: [],
+        permissionLevel:0,
+        guildOnly:false
+    }
+}
