c3e4653ca3901e0393f2efba9139102351ede41b
ronthecookie <ronthecookie0101@gmail.com>
2018-01-11 15:13:27 +0200

    edit dadjoke

--- commands/dadjoke.cmd.js
+++ commands/dadjoke.cmd.js
@@ -9,12 +9,14 @@ module.exports = {
     run: async (msg, args, client) => {
         let m = await msg.channel.send(new MessageEmbed().setDescription("<a:loadingrolling:393744853684584448>"));
         request.get("https://icanhazdadjoke.com/", {
-            json:true,
             headers: {
                 Accept:"text/plain"
             }
         }, async (error, response, body) => {
-            await m.edit(new MessageEmbed().setAuthor(msg.author.tag, msg.author.displayAvatarURL()).setColor(0xadf442).setDescription(`\`${body}\``).setTitle("Dad Joke"));
+            await m.edit(new MessageEmbed().setAuthor(msg.author.tag, msg.author.displayAvatarURL())
+            .setColor(0xadf442).setDescription(`\`${body}\``).setTitle("Dad Joke").setTimestamp(new Date())
+            .setFooter("Supplied by https://icanhazdadjoke.com/")
+        );
         });
     },
     meta: {
