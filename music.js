5ed6fc81bccac1fb2c7078cbef72f6a7de7b76d0
ronthecookie <ronthecookie0101@gmail.com>
2018-01-12 12:47:07 +0200

    ahhh

--- music.js
+++ music.js
@@ -107,18 +107,16 @@ async function play(msg) {
     if (!voiceChannel) voiceChannel = await msg.member.voiceChannel.join();
     msg.guild.me.setDeaf(true);
     let sq = mg.shiftQueue();
-    mg.setDispatcher(voiceChannel.playStream(ytdl(sq.youtube.link,{filter:"audio"}),{passes:5}));
+    mg.setDispatcher(voiceChannel.playStream(ytdl(sq.youtube.link,{filter:"audioonly"}),{passes:4}));
     mg.setPlayingTitle(sq.youtube.title);
     mg.getDispatcher().on('end',reason => {
         setTimeout(()=>{
-            console.log("music ended",reason);
             let sqa = mg.queue[0];
             mg.setPlaying(false);
             if (!sqa) {
                 console.log("music ended no more songs nr");
                 voiceChannel.disconnect();
-            }
-            else {
+            } else {
                 play(msg).catch(console.error);
             } 
         },500);
