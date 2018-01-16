0f997c85fcba353fc49ed960ff40a75666dc78a3
ronthecookie <ronthecookie0101@gmail.com>
2018-01-16 19:41:02 +0200

    purge fix

--- commands/purge.cmd.js
+++ commands/purge.cmd.js
@@ -3,7 +3,7 @@ module.exports = {
         if (!msg.guild.me.hasPermission("MANAGE_MESSAGES")) return msg.channel.send(":x: Cookieblob does not have the `Manage Messages` permission.");
         if (args.length != 1) msg.channel.send(require("../util").invalidUsageEmbed(msg, "purge"));
         let count = parseInt(args[0]);
-        if (count == null) msg.channel.send(require("../util").invalidUsageEmbed(msg, "purge"));
+        if (!count || count > 100 || count < 2) msg.channel.send(require("../util").invalidUsageEmbed(msg, "purge"));
         let deleted = await msg.channel.bulkDelete(count);
         msg.channel.send(`:ok_hand: Removed ${deleted.size} messages.`).then(m => {
             m.delete(1000*30);
@@ -12,7 +12,7 @@ module.exports = {
     meta: {
         name: "purge",
         description: "Purge messages.",
-        usage: ["amount"],
+        usage: ["amount up to 100"],
         permissionLevel:"modRole",
         guildOnly:true
     }
