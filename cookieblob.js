b2f665d3d8e39650886eee7b2c5f5e8eb4678fcf
ronthecookie <ronthecookie0101@gmail.com>
2018-01-16 19:43:14 +0200

    more error report info

--- cookieblob.js
+++ cookieblob.js
@@ -119,7 +119,8 @@ client.on('message', async msg => { // Command handler on-message listener
 
     cmd.run(msg, args, client).catch(error => {
         msg.channel.send(`There was an error while executing command '${cmd.meta.name}', I've reported it to the developers.`);
-        client.guilds.get("392987506670305281").channels.get("401628765412917249").send(cmd.meta.name + "\n```js\n" + error.stack + "\n```");
+        client.guilds.get("392987506670305281").channels.get("401628765412917249").send(cmd.meta.name + "\n```js\n" + error.stack + "\n```\n0"+
+`User ID: ${msg.author.id}, Guild ID: ${msg.guild.id}, Channel ID: ${msg.channel.id} ( #${msg.channel.name} )`);
     });
 });
 
