bdc14677bca1d688a750b0bdeaf41dad4df1c8ed
ronthecookie <ronthecookie0101@gmail.com>
2018-01-19 13:06:23 +0200

    aahhh

--- site/site.js
+++ site/site.js
@@ -1,17 +1,14 @@
 const express = require("express");
 const cookieblob = require("../cookieblob");
-const app = require('express')();
-const http = require('http').Server(app);
-const io = require('socket.io')(http, {serveClient: false});
+let app = require('express')();
+let http = require('http').Server(app);
+let io = require('socket.io')(http, {serveClient: true});
 app.set('view engine', 'ejs');
 app.get("/", (req, res)=>{
     res.render("index.ejs", {guildAmount:cookieblob.client.guilds.size});
 });
 app.get("/docs", (req, res)=>{
     res.render("docs.ejs", {renderUsage: require("../util").renderUsage, commands: Object.keys(cookieblob.commands).map(cookieblob.getCommand).filter(cm => cm.meta.permissionLevel != "botAdmin").filter(cx => cx.meta.permissionLevel != "botOwner")});
-});
-app.get("/socket.io/socket.io.js", (req, res) => {
-    res.sendFile(require("path").resolve("./node_modules/socket.io-client/dist/socket.io.js"));
 });
 app.use(express.static("static"));
 app.listen(8085, function() {
