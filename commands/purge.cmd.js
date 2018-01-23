1563b631f8cef12c4a1d0c87de22ba1758f21a91
ronthecookie <ronthecookie0101@gmail.com>
2018-01-23 20:29:53 +0200

    fix purge cmd

--- commands/purge.cmd.js
+++ commands/purge.cmd.js
@@ -1,13 +1,18 @@
+const {Client, Message}  = require("discord.js");
 module.exports = {
+                    /**
+     * @param {Message} msg
+     * @param {Array<String>} args
+     * @param {Client} client
+     */
     run: async (msg, args, client) => {
         if (!msg.guild.me.hasPermission("MANAGE_MESSAGES")) return msg.channel.send(":x: Cookieblob does not have the `Manage Messages` permission.");
         if (args.length != 1) msg.channel.send(require("../util").invalidUsageEmbed(msg, "purge"));
         let count = parseInt(args[0]);
         if (!count || count > 100 || count < 2) msg.channel.send(require("../util").invalidUsageEmbed(msg, "purge"));
         let deleted = await msg.channel.bulkDelete(count);
-        msg.channel.send(`:ok_hand: Removed ${deleted.size} messages.`).then(m => {
-            m.delete(1000*30);
-        });
+        await msg.channel.send(`:ok_hand: Removed ${deleted.size} messages.`)
+        client.setTimeout(async () => {await m.delete()}, 1000*20);
     },
     meta: {
         name: "purge",
