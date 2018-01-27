d49e6a3ba3a34bf5de33f8fd4030db0ba25869d9
ronthecookie <ronthecookie0101@gmail.com>
2018-01-27 10:09:32 +0200

    fix word

--- commands/discrim.cmd.js
+++ commands/discrim.cmd.js
@@ -7,7 +7,7 @@ module.exports = {
      */
     run: async (msg, args, client) => {
         const discrim = args.length == 1 ? args[0] : msg.author.discriminator;
-        if (!(discrim.split("").every(v => !isNaN(parseInt(v))))) return await msg.channel.send(":x: A discriminator can only contain letters.");
+        if (!(discrim.split("").every(v => !isNaN(parseInt(v))))) return await msg.channel.send(":x: A discriminator can only contain digits.");
         if (discrim.length != 4) return await msg.channel.send(":x: A discriminator is always 4 digits.");
         const users = client.users.filter(user => user.discriminator == discrim).map(user => user.tag).slice(0, 9);
         let embed = new MessageEmbed()
@@ -17,7 +17,7 @@ module.exports = {
         .setTimestamp(new Date())
         .setDescription(users.join("\n"));
         await msg.channel.send(embed);
-    },
+    },  
     meta: {
         name: "discrim",
         description: "See other users with your discrim or another one.",
