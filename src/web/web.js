91c898dd0bb2e5e3ed42c138436f3801525c2579
ronthecookie <ronthecookie0101@gmail.com>
2018-02-10 14:04:06 +0200

    Setup web for jellz

---
+++ src/web/web.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+const port = process.env.port || 3000;
+app.set("view engine", "ejs");
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
+app.get("/", (req, res) => {
+    res.render("index");
+});
