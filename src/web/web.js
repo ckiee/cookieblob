5c3f3082270926f5817c5413d03c3e5b5539fc3c
ronthecookie <ronthecookie0101@gmail.com>
2018-02-13 19:17:36 +0200

    work on website

--- src/web/web.js
+++ src/web/web.js
@@ -8,4 +8,10 @@ app.listen(port, () => {
 app.get("/", (req, res) => {
     res.render("index");
 });
+app.get("/invite", (req, res) => {
+    res.redirect("https://discordapp.com/oauth2/authorize?client_id=324874714646577152&scope=bot&permissions=3173376");
+});
 app.use(express.static("static"));
+app.use((req, res) => {
+    res.render("error", {error:"404 Page not found."});
+});
