02300cd80d93a37529498bba290486858593f635
Jellz <xdjellz@gmail.com>
2018-02-17 09:26:06 +1100

    small stats api endpoint

--- src/web/web.js
+++ src/web/web.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
+const Cookieblob = require("../Cookieblob");
 app.set("view engine", "ejs");
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
@@ -12,6 +13,11 @@ app.get("/invite", (req, res) => {
     res.redirect("https://discordapp.com/oauth2/authorize?client_id=324874714646577152&scope=bot&permissions=3173376");
 });
 app.use(express.static("static"));
+
+app.get('/bstats', (req, res) => {
+    res.status(200).json({users: cookieblob.users.size, guilds: cookieblob.guilds.size, channels: cookieblob.channels.size, tag: cookieblob.user.tag, id: cookieblob.user.id});
+});
+
 app.use((req, res) => {
     res.render("error", {error:"404 Page not found."});
 });
