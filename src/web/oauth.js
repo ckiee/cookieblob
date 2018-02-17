65cab2efeef5634cb4d289344c7691590e8b6992
ronthecookie <ronthecookie0101@gmail.com>
2018-02-17 10:14:52 +0200

    ooooooauth

--- src/web/oauth.js
+++ src/web/oauth.js
@@ -5,13 +5,15 @@ app.get("/", passport.authenticate("discord", { scope: scopes }), (req, res) => 
 });
 
 app.get("/callback",
-    passport.authenticate("discord", { failureRedirect: "/" }), (req, res) => res.redirect("/oauthguard/dashboard"));
+    passport.authenticate("discord", { failureRedirect: "/" }), (req, res) => res.redirect("/oauth/dashboard"));
 
 app.get("/logout", (req, res) => {
     req.logout();
     res.redirect("/");
 });
-
+app.get("/debug", (req, res) => {
+    if (req.isAuthenticated()) 
+});
 app.use((req, res, next) => {
     if (req.isAuthenticated()) return next();
     res.redirect("/oauthguard");
