d178b9dea0770bd23ca8e4d545c07cefa1865a47
ronthecookie <ronthecookie0101@gmail.com>
2018-02-22 07:40:38 +0200

    eval filter all conf props

--- src/commands/eval.cmd.js
+++ src/commands/eval.cmd.js
@@ -1,5 +1,17 @@
 const Cookieblob = require("../Cookieblob");
 const Permissions = require("../Permissions");
+/**
+ * @param {Cookieblob} cookieblob 
+ * @param {String} str
+ * @returns {String} 
+ */
+function filter(cookieblob, str) {
+    const r = "[redacted]";
+    Object.keys(cookieblob.config).forEach(k => {
+        r = r.split(cookieblob.config[k]).join(r);
+    });
+    return r;
+}
 module.exports = {
     /**
      * @param {Cookieblob} cookieblob
@@ -13,10 +25,10 @@ module.exports = {
             if (typeof result != "string") result = require("util").inspect(result);
             if (result.length > 1990) return msg.channel.send("Message is over the discord message contents limit.");
             if (result instanceof Promise) result = await result;
-            await msg.channel.send("```js\n"+result+"\n```");
+            await msg.channel.send("```js\n"+filter(result)+"\n```");
         } catch (error) {
             console.log(`Error while ${msg.author.tag} ran eval.\n${error.stack}`);
-            msg.channel.send("Error!```"+error.stack+"```");
+            await msg.channel.send("Error!```"+filter(error.stack)+"```");
         }
     },
     name: "eval",
