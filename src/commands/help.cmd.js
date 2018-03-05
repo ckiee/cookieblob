9886a25cb448fd32116b4beeca5e9de5b8d97724
ronthecookie <ronthecookie0101@gmail.com>
2018-03-05 10:19:01 +0200

    ahh help lets disable it and online docs

--- src/commands/help.cmd.js
+++ src/commands/help.cmd.js
@@ -8,42 +8,11 @@ module.exports = {
      * @param {String[]} args
      */
     run: async (cookieblob, msg, args) => {
-        if (args.length < 1) args[0] = 1;
-        if (args[0] < 1) return msg.channel.send(":x: Pages start from `1`!");
-        const abandonTime = 40000;//ms
-        const cpp = 10; // commands per page
-        const commands = Object.keys(cookieblob.commands).map(cookieblob.commands.get).filter(cm => cm.permissionLevel !== Permissions.botOwner).filter(cx => cx.permissionLevel !== Permissions.botDeveloper);
-        let currentPage = args[0] - 1;
-        if (isNaN(currentPage)) {
-            require("../util").usage(msg);
-            return;
-        }
-        function getLastProperPage() {
-            let amount = commands.length;
-            let result = commands.length/cpp;
-            if (commands.length%cpp != 0) result++;
-            return ~~result;
-        }
-        if (currentPage > getLastProperPage()) return msg.channel.send(`Invalid page! Range is 1 to ${getLastProperPage()}`);
-        async function makeEmbed() {
-            let startFrom = currentPage*cpp;
-            let pageCmds = commands.slice(startFrom, startFrom + cpp);
-            const viewthedocs = "***[View the documentation online](https://cookieblob.ronthecookie.me/docs)***";
-            let embed = new MessageEmbed()
-            .setAuthor("Cookieblob command list - Page "+(currentPage+1),cookieblob.user.displayAvatarURL())
-            .setColor(0xffc300)
-            .setTimestamp(new Date())
-            .setDescription(viewthedocs+"\n"
-            +pageCmds.map(cmd => `**${cmd.name}** - ${cmd.description}\nUsage: ${cmd.formatCommand()}`).join("\n\n")
-        + "\n" + viewthedocs);
-            return embed;
-        }
-        let emB = await makeEmbed();
-        let m = await msg.channel.send(emB);
+        await msg.channel.send("We've disabled the help command, please view the documentation instead: https://cookieblob.ronthecookie.me/docs"); 
     },
     name: "help",
-    description: "Show the list of commands you are currently looking at!",
-    usage: ["page:number"],
+    description: "Use https://cookieblob.ronthecookie.me/docs instead.",
+    usage: [],
     permissionLevel:Permissions.everyone,
     guildOnly:false
 }
