8bf0289126d8cfe02f5f7292558abc3200991e0d
ronthecookie <ronthecookie0101@gmail.com>
2018-06-18 17:33:35 +0300

    attempt fix

--- src/commands/catfact.cmd.js
+++ src/commands/catfact.cmd.js
@@ -11,8 +11,8 @@ module.exports = {
      * @param {String[]} args
      */
     run: async (cookieblob, msg, args) => {
+        await cookieblob.commands.get("random").run(cookieblob, msg, ["catfact"]);
         msg.channel.send(":warning: This usage of the command will be removed in the future, use `cb!random catfact` instead.");
-        cookieblob.commands.get("random").run(cookieblob, msg, ["catfact"]);
     },
     name: `catfact`,
     description: `Show a random cat fact!`,
