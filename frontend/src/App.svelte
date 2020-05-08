565b66fffee0ef16b895b2d224729b36058d91a2
Ron B <me@ronthecookie.me>
2020-05-08 23:55:29 +0300

    frontend: format App.svelte

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,6 +1,6 @@
 <script>
-  import Home from "./routes/Home.svelte";
-  import { Router, Link, Route } from "svelte-routing";
+    import Home from "./routes/Home.svelte";
+    import { Router, Link, Route } from "svelte-routing";
 </script>
 
 <style>
@@ -8,10 +8,10 @@
 </style>
 
 <main class="h-screen bg-darker text-white">
-  <Router url={window.location.pathname}>
-    <!-- <nav>
+    <Router url={window.location.pathname}>
+        <!-- <nav>
       <Link to="/" class="text-link">Home</Link>
     </nav> -->
-    <Route path="/" component={Home} />
-  </Router>
+        <Route path="/" component={Home} />
+    </Router>
 </main>
