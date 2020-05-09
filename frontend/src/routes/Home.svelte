2a6c9d8eaff27ffa2f6f3a4b50b172b18734ba3f
Ron B <me@ronthecookie.me>
2020-05-09 17:37:01 +0300

    chore(frontend): remove old import

--- frontend/src/routes/Home.svelte
+++ frontend/src/routes/Home.svelte
@@ -1,7 +1,3 @@
-<script>
-    import "/assets/discord-logo-blurple.jpg";
-</script>
-
 <div class="h-screen flex flex-col justify-center">
     <a href="/api/auth" class="bg-dark mx-auto p-4">
         <p class="text-center text-3xl">Cookieblob Login</p>
