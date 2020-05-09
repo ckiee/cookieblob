ce242b2f41f1cf4cdaf3b7ad0fc14c2294592002
Ron B <me@ronthecookie.me>
2020-05-09 16:53:10 +0300

    chore(frontend): switch to svg instead of png

--- frontend/src/routes/Home.svelte
+++ frontend/src/routes/Home.svelte
@@ -3,8 +3,11 @@
 </script>
 
 <div class="h-screen flex flex-col justify-center">
-    <a href="/dash" class="bg-dark mx-auto p-4">
+    <a href="/api/auth" class="bg-dark mx-auto p-4">
         <p class="text-center text-3xl">Cookieblob Login</p>
-        <img src="/assets/discord-logo-blurple.jpg" alt="Discord Logo" />
+        <img
+            class="w-64"
+            src="https://discordapp.com/assets/192cb9459cbc0f9e73e2591b700f1857.svg"
+            alt="Discord Logo" />
     </a>
 </div>
