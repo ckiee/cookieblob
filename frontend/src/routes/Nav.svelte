42e0eefb7ea4040508167e558405bb2ecb7919db
Ron B <me@ronthecookie.me>
2020-05-10 14:45:10 +0300

    feat(frontend): add logout

--- frontend/src/routes/Nav.svelte
+++ frontend/src/routes/Nav.svelte
@@ -1,7 +1,26 @@
-<nav class="flex gap-10 items-center">
-    <a href="/" class="flex items-center gap-2">
-        <img src="/assets/ablobnomcookie.gif" alt="animated blob nom cookie" />
-        <p class="text-xl">Cookieblob</p>
-    </a>
-    <a href="/dashboard">Dashboard</a>
-</nav>
+<script>
+    import { user, logOut } from "../stores";
+</script>
+
+<div class="flex content-between">
+
+    <div class="flex gap-10 items-center w-full">
+        <a href="/" class="flex items-center gap-2">
+            <img
+                src="/assets/ablobnomcookie.gif"
+                alt="animated blob nom cookie" />
+            <p class="text-xl">Cookieblob</p>
+        </a>
+        <a href="/dashboard">Dashboard</a>
+
+    </div>
+    {#if $user}
+        <div class="flex gap-10 items-center justify-end w-full">
+            <a href="#" on:click={logOut}>Log out</a>
+            <img
+                class="rounded-full"
+                src="https://cdn.discordapp.com/avatars/{$user._id}/{$user.avatar}.png?size=64"
+                alt="User Avatar" />
+        </div>
+    {/if}
+</div>
--- frontend/src/stores.js
+++ frontend/src/stores.js
@@ -1,6 +1,12 @@
-import { readable } from "svelte/store";
+import { readable, writable } from "svelte/store";
 
-export const user = readable(null, async (set) => {
-    set(await (await fetch("/api/users/me")).json());
+export const user = writable(null, async (set) => {
+    const res = await fetch("/api/users/me");
+    set(res.ok ? await res.json() : null);
     return () => {};
 });
+
+export async function logOut() {
+    await fetch("/api/auth/logout", { method: "POST", credentials: "include" });
+    user.set(null);
+}
