d8950786bb5bb2e64e5e7e2f02a647b52b6f85a5
Ron B <me@ronthecookie.me>
2020-05-10 11:42:36 +0300

    chore(frontend): organize palette

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,10 +3,10 @@ module.exports = {
     theme: {
         extend: {
             colors: {
-                blurple: "#7289DA",
-                greyple: "#99AAB5",
+                blpl: "#7289DA",
+                grpl: "#99AAB5",
                 dark: "#2C2F33",
-                darker: "#23272A",
+                nqbl: "#23272A",
                 link: "#00b0f4"
             }
         }
